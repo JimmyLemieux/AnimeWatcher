9492a4cf4dfd67fb53b18b94d66d9ad71900e2ff
James Lemieux <jimmylemieux9@gmail.com>
2021-02-21 16:14:31 -0500

    changed back to browser parse

--- main/Scrape.py
+++ main/Scrape.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from Helper import *
 import Constant
+import time
 
 class Scrape:
     def __init__(self, numPages, AnimeDB):
@@ -13,12 +14,19 @@ class Scrape:
             URL = Constant.ANIME_URL + "{0}".format(i+1)
             print ('Scraping page ' + URL)
             shows = self.scrapeShowLogic(URL)
+            print ("Starting db stuff")
             for show in shows:
                 print (show)
+                continue
                 sql = "INSERT INTO providedShows (showTitle, showURL) VALUES (%s, %s)"
                 val = (show["showTitle"], show["showURL"])
                 cursor.execute(sql, val)
                 AnimeDB.commit()
+
+                rowId = cursor.lastrowid()
+                for episode in show['episodes']:
+                    sql = "INSERT INTO providedEpisodes (episodeName, episodeIFrame, showID) VALUE (%s, %s, %d)"
+                    val = (episode)
                 print(cursor.rowcount, "record inserted.")
         pass
 
@@ -37,25 +45,30 @@ class Scrape:
             print ("Couldn't find the shows page")
             return
         
-        for counter, item in enumerate(listItems):
-
-            if (counter == 10):
-                break
+        for _, item in enumerate(listItems):
             show = {}
             showTitle = Helper().encodeString(item.text)
 
             showURL = Helper().showUrlMod(item)
             print (showURL)
 
+            showPage = requests.get(showURL).content
+            animeSoup = BeautifulSoup(showPage, 'html.parser')
+
+            imageDiv = animeSoup.find('div', attrs={'class': 'anime_info_body_bg'})
+            show['showImg'] = imageDiv.find('img')['src']
+
             show['showURL'] = showURL
             
             #Here since we now have the url that will take us to the episodes we can then call the code that will handle this page
             episodeLinks = self.scrapeShowEpisodeLogic(showURL)
+            print (episodeLinks)
 
             videoLinks = []
 
             for link in episodeLinks:
                 videoLink = self.scrapeEpisodeVideo(link)
+                # print (videoLink)
                 videoLinks.append(videoLink)
 
 
@@ -72,6 +85,7 @@ class Scrape:
             show['meta'] = meta
             shows.append(show)
             # this should to a database
+            print (show)
     
         print ("#### DONE ####")
         return shows
@@ -79,7 +93,8 @@ class Scrape:
     #This is for when we actuallly want to get the video embed link...
     def scrapeEpisodeVideo(self, url):
         website = requests.get(url).content
-        soup = BeautifulSoup(website, "html.parser")
+
+        soup = BeautifulSoup(website, 'html.parser');
         #I need to know find a div with id load_anime
         animeDiv = soup.find("div", attrs={"id":"load_anime"})
         if (not animeDiv): return ""
@@ -96,30 +111,33 @@ class Scrape:
 
         episodes = []
         #The url here is for each of the internal episodes for the show
-        # options = Options()
-        # options.headless = True
+        options = Options()
+        options.headless = False
 
-        # browser = webdriver.Firefox(options=options)
-        # print ("here")
-        # browser.get(url)
-        # browser.implicitly_wait(2)
+        browser = webdriver.Chrome(executable_path="./bin/chromedriver",options=options)
+        browser.get(url)
+        browser.implicitly_wait(5000)
 
         #Use the BSoup parser here to speed this up
-        bs_obj = BeautifulSoup(website, 'html.parser')
 
+        #bs_obj = BeautifulSoup(website, 'html.parser')
         try:
-            parent = bs_obj.find(id='load_ep')
-            links = parent.findAll("a")
+            parent = browser.find_element_by_id(id='episode_related')
+            print("debug")
+            #parent = bs_obj.find(id='load_ep')
+            links = parent.find_element_by_tag_name("a")
+            #links = parent.findAll("a")
         except:
             print ('An error with the ep with selenium')
             return
 
         for link in links:
             try:
-                episodeLink = link.find('href').text
+                episodeLink = link.get_attribute('href')
+                #episodeLink = link.find('href').text
                 episodes.append(Helper().encodeString(episodeLink))
             except:
-                print ("Erorrs")
+                print ("Error")
                 pass
-        # browser.close()
+        browser.close()
         return episodes
